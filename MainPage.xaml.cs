26f5e7f9745878fa3fb74aac8a3f0e30e7e705b8
tgorman31 <gorman.thomas31@gmail.com>
2025-02-26 18:19:11 +1300

    Added code to handle mouse clicks on game button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -33,10 +33,39 @@
                 animalEmoji.RemoveAt(index);
             }
         }
-
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
         private void Button_Clicked(object sender, EventArgs e)
         {
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
 
+            if (matchesFound == 8)
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
         }
     }
 
